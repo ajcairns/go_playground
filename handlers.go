3902a63b3ba2706ccdb0492ae2dd0325568633ef
ajcairns <anitajcairns@gmail.com>
2017-10-30 15:47:26 +0000

    Create mock repo

--- handlers.go
+++ handlers.go
@@ -13,15 +13,15 @@ func Index(w http.ResponseWriter, r *http.Request) {
 }
 
 func TodoIndex(w http.ResponseWriter, r *http.Request) {
-	todos := Todos{
-		Todo{Name: "Write presentation"},
-		Todo{Name: "Host meetup"},
-	}
-
+	
+	w.Header().Set("Content-Type", "application/json;charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+	
 	if err := json.NewEncoder(w).Encode(todos); err != nil {
 		panic(err)
 	}
 }
+
 
 func TodoShow(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
